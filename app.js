c5f84b65a8ec77e21e011f1f1049fa8099dd2e7f
Sreya <sreya.ku@inapp.com>
2023-04-25 15:20:29 +0530

    Adding

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('Hello world!');
 });
-
 app.listen(3000, function () {
   console.log('Example app listening on port ----3000');
 });
